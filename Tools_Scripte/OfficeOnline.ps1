603ba626ba97bac374803c04f69a9310b4c0dad8
RauchendRaser <andreasr@ppedv.de>
2021-05-28 15:21:30 +0200

    Tools und Scripte

---
+++ Tools_Scripte/OfficeOnline.ps1
@@ -0,0 +1,13 @@
+﻿New-SPWOPIBinding -ServerName <WacServerName> -AllowHTTP
+Get-SPWOPIZone
+Set-SPWOPIZone -zone "internal-http"
+(Get-SPSecurityTokenServiceConfig).AllowOAuthOverHttp
+$config = (Get-SPSecurityTokenServiceConfig)
+$config.AllowOAuthOverHttp = $true
+$config.Update()
+(Get-SPSecurityTokenServiceConfig).AllowOAuthOverHttp
+
+#EXCEL SOAP API
+$Farm = Get-SPFarm 
+$Farm.Properties.Add("WopiLegacySoapSupport", "<URL>/x/_vti_bin/ExcelServiceInternal.asmx"); 
+$Farm.Update();
